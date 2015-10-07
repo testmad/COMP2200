6b5076bb0fe80e6a4cad94a95fdfb1db449fa369
testmad <testmad@thinstack.net>
2015-10-07 17:43:53 -0400

    fixes

--- Project3/src/GUI_WorkOrder.java
+++ Project3/src/GUI_WorkOrder.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
@@ -47,9 +48,6 @@ class MyFrameClass extends JFrame implements ActionListener
 		cp = getContentPane();
 		cp.add(mainPanel, BorderLayout.CENTER);
 		
-		//newDialog = new MyDialog(this);
-		//editDialog = new MyDialog(this, "entry");
-		
 		getRootPane().setDefaultButton(addBtn);
 		
 		setupMainFrame();
@@ -74,24 +72,44 @@ class MyFrameClass extends JFrame implements ActionListener
 	{
 		if(event.getActionCommand().equals("ADD"))
 		{
+			newDialog = new MyDialog(this);
 			newDialog.setLocationRelativeTo(this);
 			newDialog.setVisible(true);
 		}
 		else if(event.getActionCommand().equals("EDIT"))
 		{
-			//editDialog.setLocationRelativeTo(this);
-			editDialog = new MyDialog(this, "meh");
+			editDialog = new MyDialog(this, WorkOrder.debugLoad());
 			editDialog.setLocationRelativeTo(this);
-			editDialog.
 			editDialog.setVisible(true);
 		}
 	}
 }
 
+//class DeptVerifier extends InputVerifier
+//{
+//	
+//	public DeptVerifier()
+//	{
+//	}
+//	
+//	@Override
+//	public boolean verify(JComponent input)
+//	{
+//		int dSelect;
+//		dSelect = ((JComboBox)input).getSelectedIndex();
+//		
+//		if(dSelect <=0)
+//			return false;
+//		else
+//			return true;
+//	}
+//	
+//}
+
 class MyDialog extends JDialog implements ActionListener
 {
 	String[] labelNames = {"Name:", "Department:", "Date Ordered:", "Date Filled:", "Description:", "Billing Rate:"};
-	String[] comboItems = {"SALES", "HARDWARE", "ELECTRONICS"}; 
+	String[] comboItems = {"Select...", "SALES", "HARDWARE", "ELECTRONICS"}; 
 	
 	private JTextField nameField;
 	private JComboBox<String> deptCombobox;
@@ -115,10 +133,17 @@ class MyDialog extends JDialog implements ActionListener
 		this.add(saveAddBtn);
 	}
 	
-	public MyDialog(JFrame aFrame, String str)
+	public MyDialog(JFrame aFrame, WorkOrder wo)
 	{
 		super(aFrame, "Edit Work Order");
 		addForm();
+		
+		this.nameField.setText(wo.name);
+		this.deptCombobox.setSelectedIndex(wo.department);
+		this.dateInField.setText(wo.dateIn);
+		this.dateOutField.setText(wo.dateOut);
+		this.descField.setText(wo.description);
+		this.rateField.setText(Float.toString(wo.rate));
 		
 	}
 	
@@ -145,16 +170,21 @@ class MyDialog extends JDialog implements ActionListener
 		
 		this.add(nameLbl);
 		this.add(nameField);
+		nameField.setInputVerifier(null);
 		this.add(deptLbl);
 		this.add(deptCombobox);
+		//deptCombobox.setInputVerifier(new DeptVerifier());
 		this.add(dateInLbl);
 		this.add(dateInField);
+		dateInField.setInputVerifier(null);
 		this.add(dateOutLbl);
 		this.add(dateOutField);
+		dateOutField.setInputVerifier(null);
 		this.add(descLbl);
 		this.add(descField);
 		this.add(rateLbl);
 		this.add(rateField);
+		rateField.setInputVerifier(null);
 		
 		JButton cancelBtn;
 		JButton saveCloseBtn;
@@ -182,12 +212,17 @@ class MyDialog extends JDialog implements ActionListener
 	public void actionPerformed(ActionEvent event)
 	{
 		if(event.getActionCommand().equals("CANCEL"))
+		{
 			System.out.println("clicked cancel");
+			dispose();
+		}
+		
 		else if(event.getActionCommand().equals("SAVECLOSE"))
 		{
 			System.out.println("clicked save and close");
 			
 			new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), Float.parseFloat(rateField.getText())).debugSave();
+			dispose();
 		}
 			
 		else if(event.getActionCommand().equals("SAVEANOTHER"))
@@ -211,18 +246,19 @@ class WorkOrder
 	
 	public WorkOrder(DataInputStream dis)
 	{
-		try {
+		try
+		{
 			name = dis.readUTF();
 			department = dis.readInt();
 			dateIn = dis.readUTF();
 			dateOut = dis.readUTF();
 			description = dis.readUTF();
 			rate = dis.readFloat();
-		} catch (IOException e) {
-			
+		}
+		catch (IOException e)
+		{			
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
 		}
-		
 	}
 	
 	public WorkOrder(String strName, int dept, String date1, String date2, String desc, float pRate)
@@ -233,21 +269,22 @@ class WorkOrder
 		dateOut = date2;
 		description = desc;
 		rate = pRate;
-		
 	}
 	
 	void loadOrder(DataInputStream dis)
 	{
 		System.out.println("LOADING DATA.");
-		try {
+		try
+		{
 			name = dis.readUTF();
 			department = dis.readInt();
 			dateIn = dis.readUTF();
 			dateOut = dis.readUTF();
 			description = dis.readUTF();
 			rate = dis.readFloat();
-		} catch (IOException e) {
-			
+		}
+		catch (IOException e)
+		{	
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
 		}
 	}
@@ -255,51 +292,66 @@ class WorkOrder
 	void saveOrder(DataOutputStream dos)
 	{
 		System.out.println("SAVING DATA.");
-		try {
+		try
+		{
 			dos.writeUTF(name);
 			dos.writeInt(department);
 			dos.writeUTF(dateIn);
 			dos.writeUTF(dateOut);
 			dos.writeUTF(description);
 			dos.writeFloat(rate);
-		} catch (IOException e) {
-			
+		}
+		catch (IOException e)
+		{	
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	
-	void debugLoad()
+	static WorkOrder debugLoad()
 	{
 		Random r = new Random();
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		name = "" + randString(r.nextInt(10)) + " " + randString(r.nextInt(10));
-		department = r.nextInt(3);
-		dateIn = LocalDate.parse((CharSequence) new Date()).toString();
-		dateOut = LocalDate.parse((CharSequence) new Date()).plusDays(r.nextInt(20)).toString();
-		description = "" + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10));
-		rate = r.nextFloat() * r.nextInt(5);
+		String tmpdate = sdf.format(cal.getTime());
+		
+		cal.add(Calendar.DATE, r.nextInt(31));
+		String tmpdate2 = sdf.format(cal.getTime());
+		
+		String tmpname = "" + randString(r.nextInt(10)) + " " + randString(r.nextInt(10));
+		int tmpdepartment = r.nextInt(3)+1;
+		String tmpdateIn = tmpdate;
+		String tmpdateOut = tmpdate2;
+		String tmpdescription = "" + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10));
+		Float tmprate = r.nextFloat() * r.nextInt(5);
+		
+		WorkOrder tmp = new WorkOrder(tmpname, tmpdepartment, tmpdateIn, tmpdateOut, tmpdescription, tmprate);
+		
+		return tmp;
 	}
 	
 	void debugSave()
 	{
 		String newLine = System.getProperty("line.separator");
 		
-		System.out.println(name + newLine + department + newLine + dateIn + newLine + dateOut + newLine + description + newLine + rate);
+		System.out.println(	name + newLine +
+							department + newLine +
+							dateIn + newLine +
+							dateOut + newLine +
+							description + newLine +
+							rate);
 	}
 	
-	String randString(int characters)
-	{	
-		
+	static String randString(int characters)
+	{
 		Random r = new Random();
-		
 		String temp = "" + (char)(r.nextInt(26) + 'A');
 		
 		for(int i = 0; i < characters; i++)
 		{
 			temp += "" + (char)(r.nextInt(26) + 'a');
 		}
-		
 		return temp;
-				 
 	}
 }
+
