73a63d65c9f8efef8151e496e96958611391f0ee
testmad <testmad@thinstack.net>
2015-10-08 00:03:54 -0400

    missing consistency check and group layout

--- Project3/src/GUI_WorkOrder.java
+++ Project3/src/GUI_WorkOrder.java
@@ -1,13 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Random;
+import java.io.*;
+import java.text.*;
+import java.util.*;
 
 import javax.swing.*;
 
@@ -55,11 +50,6 @@ class MyFrameClass extends JFrame implements ActionListener
 	
 	void setupMainFrame()
 	{
-		Toolkit tk;
-		Dimension d;
-		
-		tk = Toolkit.getDefaultToolkit();
-				
 		setTitle("WorkOrderDialog");
 		setMinimumSize(new Dimension(306, 124));
 		setLocationRelativeTo(null);
@@ -78,33 +68,124 @@ class MyFrameClass extends JFrame implements ActionListener
 		}
 		else if(event.getActionCommand().equals("EDIT"))
 		{
+			//Uncomment for random generated work order.
 			editDialog = new MyDialog(this, WorkOrder.debugLoad());
+			
+			//Uncomment for loading from file.
+//			FileInputStream fis = null;
+//			try
+//			{
+//				fis = new FileInputStream("workorder.dat");
+//			}
+//			catch (FileNotFoundException e)
+//			{
+//				JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+//			}
+//			
+//          DataInputStream dis = new DataInputStream(fis);
+//          editDialog = new MyDialog(this, new WorkOrder().loadOrder(dis));
+//			dis.close();
+//			fis.close();
+            
 			editDialog.setLocationRelativeTo(this);
 			editDialog.setVisible(true);
 		}
 	}
 }
 
-//class DeptVerifier extends InputVerifier
-//{
-//	
-//	public DeptVerifier()
-//	{
-//	}
-//	
-//	@Override
-//	public boolean verify(JComponent input)
-//	{
-//		int dSelect;
-//		dSelect = ((JComboBox)input).getSelectedIndex();
-//		
-//		if(dSelect <=0)
-//			return false;
-//		else
-//			return true;
-//	}
-//	
-//}
+class TextVerifier extends InputVerifier
+{
+	public TextVerifier()
+	{
+	}
+	
+	public boolean verify(JComponent input)
+	{
+		return true;
+	}
+}
+
+class RateVerifier extends InputVerifier
+{
+	public RateVerifier()
+	{
+	}
+	
+	public boolean verify(JComponent input)
+	{
+		if(((JTextField)input).getText().trim().equals(""))
+		{
+			System.out.println("Empty Rate.");
+			return true;
+		}
+		else
+		{
+			try
+			{
+				DecimalFormat decim = new DecimalFormat("0.00");
+				Double tmp1 = Double.parseDouble(((JTextField)input).getText().trim().replaceAll(",",""));
+				String tmp2 = decim.format(tmp1);
+				((JTextField)input).setText(tmp2);
+			}
+			catch(NumberFormatException nfe)
+			{
+				JOptionPane.showMessageDialog(null, "Invalid rate amount." + System.getProperty("line.separator") + "Amount example: 3,012.50 or 3012.5", "Error!", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+		return true;
+		}
+	}
+}
+
+class DateVerifier extends InputVerifier
+{
+	public DateVerifier()
+	{
+	}
+	
+	public boolean verify(JComponent input)
+	{
+		DateFormat df1 = new SimpleDateFormat("MM/dd/yy");
+		DateFormat df2 = new SimpleDateFormat("MM-dd-yy");
+		DateFormat df3 = new SimpleDateFormat("MM-dd-yyyy");
+		
+		df1.setLenient(false);
+		df2.setLenient(false);
+		
+		String strDate;
+		strDate = ((JTextField)input).getText().trim();
+		
+		Date date;
+		
+		if(strDate.equals(""))
+		{
+			System.out.println("Empty Date.");
+			return true;
+		}
+		else
+		{
+			try
+			{
+				date = df1.parse(strDate);
+				((JTextField)input).setText(df3.format(date));
+			}
+			catch(ParseException e1)
+			{
+				try
+				{
+					date = df2.parse(strDate);
+					((JTextField)input).setText(df3.format(date));
+				}
+				catch(ParseException e2)
+				{
+					JOptionPane.showMessageDialog(null, "Invalid date format." + System.getProperty("line.separator") + "Format example: 3-26-2012 or 03/26/2012", "Error!", JOptionPane.ERROR_MESSAGE);
+					return false;
+				}
+			}
+		return true;
+		}
+	}
+}
 
 class MyDialog extends JDialog implements ActionListener
 {
@@ -143,8 +224,7 @@ class MyDialog extends JDialog implements ActionListener
 		this.dateInField.setText(wo.dateIn);
 		this.dateOutField.setText(wo.dateOut);
 		this.descField.setText(wo.description);
-		this.rateField.setText(Float.toString(wo.rate));
-		
+		this.rateField.setText(wo.rate);
 	}
 	
 	void addForm()
@@ -168,23 +248,27 @@ class MyDialog extends JDialog implements ActionListener
 		descField = new JTextField();
 		rateField = new JTextField();
 		
+		TextVerifier tv = new TextVerifier();
+		DateVerifier dv = new DateVerifier();
+		RateVerifier rv = new RateVerifier();
+		
 		this.add(nameLbl);
 		this.add(nameField);
-		nameField.setInputVerifier(null);
+		nameField.setInputVerifier(tv);
 		this.add(deptLbl);
 		this.add(deptCombobox);
-		//deptCombobox.setInputVerifier(new DeptVerifier());
 		this.add(dateInLbl);
 		this.add(dateInField);
-		dateInField.setInputVerifier(null);
+		dateInField.setInputVerifier(dv);
 		this.add(dateOutLbl);
 		this.add(dateOutField);
-		dateOutField.setInputVerifier(null);
+		dateOutField.setInputVerifier(dv);
 		this.add(descLbl);
 		this.add(descField);
+		descField.setInputVerifier(tv);
 		this.add(rateLbl);
 		this.add(rateField);
-		rateField.setInputVerifier(null);
+		rateField.setInputVerifier(rv);
 		
 		JButton cancelBtn;
 		JButton saveCloseBtn;
@@ -205,7 +289,12 @@ class MyDialog extends JDialog implements ActionListener
 	
 	void clearData()
 	{
-		//clear the form data.
+		nameField.setText("");
+		deptCombobox.setSelectedIndex(0);
+		dateInField.setText("");
+		dateOutField.setText("");
+		descField.setText("");
+		rateField.setText("");
 	}
 
 	@Override
@@ -221,7 +310,28 @@ class MyDialog extends JDialog implements ActionListener
 		{
 			System.out.println("clicked save and close");
 			
-			new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), Float.parseFloat(rateField.getText())).debugSave();
+			//Uncomment for debug saving.
+			new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), rateField.getText()).debugSave();
+			
+			//Uncomment for real saving.
+//			FileOutputStream fos = null;
+//			
+//			try
+//			{
+//				fos = new FileOutputStream("workorder.dat");
+//			}
+//			catch (FileNotFoundException e)
+//			{
+//				JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+//			}
+//			
+//          DataOutputStream dos = new DataOutputStream(fos);
+//          new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), rateField.getText()).saveOrder(dos);
+//			fos.flush();
+//			fos.close();
+//			dos.close();
+			
+			
 			dispose();
 		}
 			
@@ -229,7 +339,27 @@ class MyDialog extends JDialog implements ActionListener
 		{
 			System.out.println("clicked save and add");
 			
-			new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), Float.parseFloat(rateField.getText())).debugSave();
+			//Uncomment for debug saving.
+			new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), rateField.getText()).debugSave();
+			
+			//Uncomment for real saving.
+//			FileOutputStream fos = null;
+//			
+//			try
+//			{
+//				fos = new FileOutputStream("workorder.dat");
+//			}
+//			catch (FileNotFoundException e)
+//			{
+//				JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+//			}
+//			
+//          DataOutputStream dos = new DataOutputStream(fos);
+//          new WorkOrder(nameField.getText(), deptCombobox.getSelectedIndex(), dateInField.getText(), dateOutField.getText(), descField.getText(), rateField.getText()).saveOrder(dos);
+//			fos.flush();
+//			fos.close();
+//			dos.close();
+            
 			clearData();
 		}
 	}	
@@ -237,60 +367,79 @@ class MyDialog extends JDialog implements ActionListener
 
 class WorkOrder
 {
-	String name;
-	int department;
-	String dateIn;
-	String dateOut;
-	String description;
-	float rate;
+	String name = null;
+	int department = 0;
+	String dateIn = null;
+	String dateOut = null;
+	String description = null;
+	String rate = null;
 	
-	public WorkOrder(DataInputStream dis)
+	public WorkOrder()
 	{
-		try
-		{
-			name = dis.readUTF();
-			department = dis.readInt();
-			dateIn = dis.readUTF();
-			dateOut = dis.readUTF();
-			description = dis.readUTF();
-			rate = dis.readFloat();
-		}
-		catch (IOException e)
-		{			
-			JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
-		}
+		
 	}
 	
-	public WorkOrder(String strName, int dept, String date1, String date2, String desc, float pRate)
+	//Redundant, but leaving it here to look back on.
+//	public WorkOrder(DataInputStream dis)
+//	{
+//		try
+//		{
+//			name = dis.readUTF();
+//			department = dis.readInt();
+//			dateIn = dis.readUTF();
+//			dateOut = dis.readUTF();
+//			description = dis.readUTF();
+//			rate = dis.readUTF();
+//		}
+//		catch (IOException e)
+//		{			
+//			JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+//		}
+//	}
+	
+	public WorkOrder(String strName, int dept, String date1, String date2, String desc, String tmprate)
 	{
 		name = strName;
 		department = dept;
 		dateIn = date1;
 		dateOut = date2;
 		description = desc;
-		rate = pRate;
+		rate = tmprate;
 	}
 	
-	void loadOrder(DataInputStream dis)
+	static WorkOrder loadOrder(DataInputStream dis)
 	{
+		String tmpname = null;
+		int tmpdepartment = 0;
+		String tmpdateIn = null;
+		String tmpdateOut = null;
+		String tmpdescription = null;
+		String tmprate = null;
+		
 		System.out.println("LOADING DATA.");
 		try
 		{
-			name = dis.readUTF();
-			department = dis.readInt();
-			dateIn = dis.readUTF();
-			dateOut = dis.readUTF();
-			description = dis.readUTF();
-			rate = dis.readFloat();
+			tmpname = dis.readUTF();
+			tmpdepartment = dis.readInt();
+			tmpdateIn = dis.readUTF();
+			tmpdateOut = dis.readUTF();
+			tmpdescription = dis.readUTF();
+			tmprate = dis.readUTF();
 		}
 		catch (IOException e)
 		{	
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
 		}
+		
+		WorkOrder tmp = new WorkOrder(tmpname, tmpdepartment, tmpdateIn, tmpdateOut, tmpdescription, tmprate);
+		System.out.println(tmpdepartment);
+		return tmp;
 	}
 	
 	void saveOrder(DataOutputStream dos)
 	{
+		String newLine = System.getProperty("line.separator");
+		
 		System.out.println("SAVING DATA.");
 		try
 		{
@@ -299,7 +448,7 @@ class WorkOrder
 			dos.writeUTF(dateIn);
 			dos.writeUTF(dateOut);
 			dos.writeUTF(description);
-			dos.writeFloat(rate);
+			dos.writeUTF(rate);
 		}
 		catch (IOException e)
 		{	
@@ -309,9 +458,11 @@ class WorkOrder
 	
 	static WorkOrder debugLoad()
 	{
+		DecimalFormat decim = new DecimalFormat("0.00");
 		Random r = new Random();
 		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		cal.setLenient(false);
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
 		
 		String tmpdate = sdf.format(cal.getTime());
 		
@@ -323,7 +474,7 @@ class WorkOrder
 		String tmpdateIn = tmpdate;
 		String tmpdateOut = tmpdate2;
 		String tmpdescription = "" + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10)) + " " + randString(r.nextInt(10));
-		Float tmprate = r.nextFloat() * r.nextInt(5);
+		String tmprate = decim.format(r.nextDouble() * r.nextInt(5));
 		
 		WorkOrder tmp = new WorkOrder(tmpname, tmpdepartment, tmpdateIn, tmpdateOut, tmpdescription, tmprate);
 		
